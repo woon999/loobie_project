db1fce95004bb2584ac570f7a23b3666123cf89a
jongwon <54282927+loosie@users.noreply.github.com>
2021-05-28 23:29:52 +0900

    # bootstrap, css-jumbotron 적용

--- src/main/java/toyproject/loobie/controller/HomeController.java
+++ src/main/java/toyproject/loobie/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(){
         log.info("home controller");
         return "home"; //
