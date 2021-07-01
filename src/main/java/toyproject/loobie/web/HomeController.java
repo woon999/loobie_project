978f6f423d6e6b8a88ec1e74d05a6a955754211a
jongwon <54282927+loosie@users.noreply.github.com>
2021-07-01 23:14:45 +0900

    #3-3 로그인 어노테이션 적용 LoginUser

--- src/main/java/toyproject/loobie/web/HomeController.java
+++ src/main/java/toyproject/loobie/web/HomeController.java
@@ -4,20 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import toyproject.loobie.config.auth.LoginUser;
 import toyproject.loobie.config.auth.dto.SessionUser;
-
-import javax.servlet.http.HttpSession;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String home(Model model){
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
-
+    public String home(Model model, @LoginUser SessionUser user){
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
