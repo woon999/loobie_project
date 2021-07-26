d0ed44e9de8ec20a6dd55455da80cfbfa1a87c16
jongwon <54282927+loosie@users.noreply.github.com>
2021-07-27 00:41:20 +0900

    #8-4 구독 확인 이메일 인증 테스트 코드 작성

--- src/test/java/toyproject/loobie/web/UserControllerTest.java
+++ src/test/java/toyproject/loobie/web/UserControllerTest.java
@@ -10,17 +10,20 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 import toyproject.loobie.domain.user.User;
 import toyproject.loobie.service.UserService;
+import toyproject.loobie.web.dto.UserSaveRequestDto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.then;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
+@Transactional
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,5 +49,38 @@ public class UserControllerTest {
         assertThat(user.getName()).isEqualTo(name);
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
+
+    @Test
+    public void 인증메일확인_입력값오류() throws Exception{
+        mockMvc.perform(get("/check-email-token")
+                .param("token","sadaskdasd")
+                .param("email","test@test.com"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("error"))
+                .andExpect(view().name("client/email-verified"));
+     }
+
+    @Test
+    public void 인증메일확인_입력값정상() throws Exception{
+        String email = "test@test.com";
+        String name = "loosie";
+        UserSaveRequestDto requestDto = UserSaveRequestDto.builder()
+                .email(email)
+                .name(name)
+                .build();
+        userService.save(requestDto);
+
+        User newUser = userService.findByEmail(email);
+        newUser.generateEmailToken();
+
+        mockMvc.perform(get("/check-email-token")
+                .param("token",newUser.getEmailCheckToken())
+                .param("email",newUser.getEmail()))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeDoesNotExist("error"))
+                .andExpect(model().attributeExists("name"))
+                .andExpect(view().name("client/email-verified"));
+    }
+
 
 }
